72c28a9bcf2a904a59aaa7bfe87a6b2db27a363c
Quang Le <iamquang95@gmail.com>
2022-10-05 00:57:33 +0700

    feat: render a tree

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 mod dir;
 mod tree;
 
-use walkdir::{DirEntry, WalkDir};
-
 use crate::dir::Dir;
 use crate::tree::{Arena, NodeId};
 use clap::Parser;
@@ -15,47 +13,10 @@ struct Args {
     path: String,
 }
 
-fn is_dir(entry: &DirEntry) -> anyhow::Result<bool> {
-    let metadata = entry.metadata()?;
-    Ok(metadata.is_dir())
-}
-
-fn traverse_dir(
-    tree: &mut Arena<Dir>,
-    node_id: NodeId,
-    path: &str,
-    depth: u64,
-) -> anyhow::Result<()> {
-    let dir = WalkDir::new(path).max_depth(1);
-    for entry in dir {
-        let entry = entry?;
-        let is_folder = is_dir(&entry)?;
-        let file_name = entry.file_name().to_str().unwrap();
-
-        let new_dir = Dir::new(file_name.to_owned(), is_folder, true);
-        let new_node_id = tree.new_node(new_dir, node_id)?;
-
-        if is_folder {
-            let entry_path = format!("{}", entry.path().display());
-            if &entry_path != &path {
-                traverse_dir(tree, new_node_id, &entry_path, depth + 1)?;
-            }
-        }
-    }
-    Ok(())
-}
-
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
-    let root = Dir::new(args.path.clone(), true, true);
-    let mut tree = Arena::<Dir>::new(root);
-    let root_id = tree.root_id();
-    traverse_dir(&mut tree, root_id, &args.path, 0)?;
-    dbg!(tree.nodes.len());
-    let root = tree.get_node(0).unwrap();
-    for node in &root.children {
-        dbg!(tree.get_node(*node));
-    }
+    let mut tree = Dir::build_dir(&args.path)?;
+    Dir::render_tree(&tree)?;
     Ok(())
 }
